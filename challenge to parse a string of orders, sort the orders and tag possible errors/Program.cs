0b810cba636021c6ace369b18a6062826a94f433
OuwaCavern <emircan.acikgoz@hotmail.com>
2024-02-18 14:20:06 +0300

    update

--- challenge to parse a string of orders, sort the orders and tag possible errors/Program.cs
+++ challenge to parse a string of orders, sort the orders and tag possible errors/Program.cs
@@ -1,2 +1,15 @@
 ﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orderStreamArray = orderStream.Split(',');
 
+Array.Sort(orderStreamArray);
+foreach(string order in orderStreamArray)
+{
+    if(order.Length != 4)
+    {
+        Console.WriteLine($"{order}\t Error!");
+    }
+    else
+    {
+    Console.WriteLine(order);
+    }
+}
