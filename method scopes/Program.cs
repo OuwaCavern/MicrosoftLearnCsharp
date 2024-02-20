85b070cd6e6b7ee586fafc8257fce02ef05017d1
Raxzero <emircan.acikgoz@hotmail.com>
2024-02-20 18:34:33 +0300

    update from my workplace

---
+++ method scopes/Program.cs
@@ -0,0 +1,52 @@
+﻿// WORKS
+/* string[] students = {"Jenna", "Ayesha", "Carlos", "Viktor"};
+
+DisplayStudents(students);
+DisplayStudents(new string[] {"Robert","Vanya"});
+
+void DisplayStudents(string[] students) 
+{
+    foreach (string student in students) 
+    {
+        Console.Write($"{student}, ");
+    }
+    Console.WriteLine();
+} */
+
+
+//Does not work because the radius is left outside of the PrintCircleArea's method scope
+/* PrintCircleArea(12);
+double circumference = 2 * pi * radius;
+void PrintCircleArea(int radius)
+{
+    double pi = 3.14159;
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+} */
+
+
+double pi = 3.14159;
+
+void PrintCircleArea(int radius)
+{
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {Math.Round(area, 2)}");
+}
+
+void PrintCircleCircumference(int radius)
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {Math.Round(circumference, 2)}");
+}
+
+
+// As long as a method is defined within the scope of your program, it can be called anywhere.
+PrintCircleInfo(12);
+PrintCircleInfo(24);
+
+void PrintCircleInfo(int radius) 
+{
+    Console.WriteLine($"Circle with radius {radius}");
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
+}
---
+++ value and reference type parameters/Program.cs
@@ -0,0 +1,45 @@
+﻿//Value types such as int, bool, float, double, and char directly contain values. Reference types such as string, array, and objects (such as instances of Random) don't store their values directly. Instead, reference types store an address where their value is being stored.
+
+//When an argument is passed to a method, value type variables have their values copied into the method. Each variable has its own copy of the value, so the original variable isn't modified.
+//With reference types, the address of the value is passed into the method. The variable given to the method references the value at that address, so operations on that variable affect the value that is referenced by the other.
+
+//It is important to remember that string is a reference type, but it is immutable. That means once it has been assigned a value, it can't be altered. In C#, when methods and operators are used to modify a string, the result that is returned is actually a new string object.
+
+// Integers are value types, which have their values copied when passed into methods. Which is why the value of c is only altered within the Multiply method. Outside of the method, c retains its original value.
+/* int a = 3;
+int b = 4;
+int c = 0;
+
+Multiply(a, b, c);
+Console.WriteLine($"global statement: {a} x {b} = {c}");
+
+void Multiply(int a, int b, int c) 
+{
+    c = a * b;
+    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+} */
+
+
+//Notice that the array remains altered outside of the Clear method scope. This happens because the Clear method updated the values stored at each address.
+int[] array = {1, 2, 3, 4, 5};
+
+PrintArray(array);
+Clear(array);
+PrintArray(array);
+
+void PrintArray(int[] array) 
+{
+    foreach (int a in array) 
+    {
+        Console.Write($"{a} ");
+    }
+    Console.WriteLine();
+}
+
+void Clear(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++) 
+    {
+        array[i] = 0;
+    }
+}
